fb20198468b974938b71fe9c64916fe10014b51b
ionone <elarahal.1972@gmail.com>
2016-03-05 22:46:32 +0100

    Añadiendo un formulario

--- src/League/VillageBundle/Controller/DefaultController.php
+++ src/League/VillageBundle/Controller/DefaultController.php
@@ -3,11 +3,60 @@
 namespace League\VillageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use League\VillageBundle\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
     public function indexAction()
     {
         return $this->render('LeagueVillageBundle:Default:index.html.twig');
     }
+
+    public function contactAction()
+    {
+        // crea una task y le asigna algunos datos ficticios para este ejemplo
+        $frmContact = new Contact();
+        $frmContact->setDate(new \DateTime('now'));
+
+        $form = $this->createFormBuilder($frmContact)
+                ->add('date', 'datetime')
+                ->add('email', 'email')
+                ->add('name', 'text')
+                ->add('text', 'textarea')
+                ->getForm();
+
+        return $this->render('LeagueVillageBundle:Default:contactnew.html.twig', array(
+                    'form' => $form->createView(),
+        ));
+    }
+
+    public function responseAction(Request $request)
+    {
+        $frmContact = new Contact();
+
+        $form = $this->createFormBuilder($frmContact)
+                ->add('name', 'text')
+                ->add('date', 'datetime', array(
+                    'widget' => 'single_text',
+                    'label' => 'Fecha',
+                ))
+                ->add('email', 'email', array(
+                    'label' => 'Correo Electrónico'
+                ))
+                ->add('text', 'textarea')
+                ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                // realiza alguna acción, tal como guardar la tarea en la base de datos
+
+                return $this->redirect($this->generateUrl('contact_success'));
+            }
+        }
+    }
+
 }
---
+++ src/League/VillageBundle/Entity/Contact.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace League\VillageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="League\VillageBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=60)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=32)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Contact
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Contact
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
